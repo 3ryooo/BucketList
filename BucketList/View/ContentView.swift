2f5f4b67b1ac2acdd1b0bad0a823fdd73062f69a
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-13 14:36:50 +0900

    finish: https://www.hackingwithswift.com/books/ios-swiftui/writing-data-to-the-documents-directory

--- BucketList/View/ContentView.swift
+++ BucketList/View/ContentView.swift
@@ -5,18 +5,65 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+
+struct Message: Codable, Identifiable {
+    var id = UUID()
+    let text: String
 }
 
+struct ContentView: View {
+    @State private var loadedMessage: Message? = nil
+    
+    var body: some View {
+        
+        Button("Read and Write") {
+            let newMessage = Message(text: "Test Message")
+            FileManager().saveText(newMessage, fileName: "message.json")
+        }
+        Button("Load") {
+            if let loaded: Message =  FileManager().loadText(fileName: "message.json") {
+                loadedMessage = loaded
+            }
+        }
+    }
+    
+    
+    
+}
+
+extension FileManager {
+    func saveText<T:Codable>(_ object: T, fileName: String) {
+        
+        do {
+            let dirURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+            let fileURL = dirURL!.appendingPathComponent(fileName)
+            let encoder = JSONEncoder()
+            encoder.outputFormatting = .prettyPrinted
+            let jsonValue = try encoder.encode(object)
+            try jsonValue.write(to: fileURL, options: [.atomic, .completeFileProtection])
+        } catch {
+            print(error)
+        }
+        
+        
+    }
+    
+    func loadText<T:Codable>(fileName: String) -> T? {
+        do {
+            let dirURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+            let fileURL = dirURL!.appendingPathComponent("message.json")
+            let data = try Data(contentsOf: fileURL)
+            let decoder = JSONDecoder()
+            let decodedData =  try decoder.decode(T.self, from: data)
+            return decodedData
+        } catch {
+            print(error)
+            return nil
+        }
+        
+    }
+    
+}
 #Preview {
     ContentView()
-}
+    }
