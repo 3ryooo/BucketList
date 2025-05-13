86cabe205a185b35c09e7377d635cdd0fb6227cb
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-13 19:53:00 +0900

    finish: https://www.hackingwithswift.com/books/ios-swiftui/downloading-data-from-wikipedia

---
+++ BucketList/Model/Result.swift
@@ -0,0 +1,20 @@
+//
+//  Result.swift
+//  BucketList
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
